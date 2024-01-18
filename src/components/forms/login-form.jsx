a45332cddd415333e96c14c4c8cfda030a58b2b6
Smahesh26 <sunainamahesh1@gmail.com>
2024-01-18 13:54:38 +0530

    update

--- src/components/forms/login-form.jsx
+++ src/components/forms/login-form.jsx
@@ -1,3 +1,175 @@
+
+// LoginForm.js
+
+import React, { useState } from "react";
+import Link from "next/link";
+import { useFormik } from "formik";
+import UserThree from "@svg/user-3";
+import Lock from "@svg/lock";
+import EyeCut from "@svg/eye-cut";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
+import useGlobalContext from "@hooks/use-context";
+import { login_schema } from "@utils/validation-schema";
+import ErrorMsg from "./error-msg";
+
+const LoginForm = () => {
+  const { loading, setLoading } = useGlobalContext();
+  const [showPass, setShowPass] = useState(false);
+  const [isCheckbox, setIsCheckbox] = useState(false);
+  const router = useRouter();
+
+  const handleChackbox = () => {
+    setIsCheckbox(!isCheckbox);
+  };
+
+  // use formik
+  const { handleChange, handleSubmit, handleBlur, errors, values, touched } =
+    useFormik({
+      initialValues: {
+        email: "",
+        password: "",
+      },
+      validationSchema: login_schema,
+      onSubmit: (values, { resetForm }) => {
+        resetForm();
+        const userInfo = {
+          email: values.email,
+          password: values.password,
+        };
+
+        axios
+          .post(`${process.env.BASE_URL}user/login`, userInfo)
+          .then((res) => {
+            setLoading(false);
+            if (res.data.data) {
+              toast.success(res.data.message);
+              localStorage.setItem("accessToken", res.data.data);
+              const redirectUrl = localStorage.getItem("redirectUrl");
+              if (redirectUrl) {
+                router.push(redirectUrl);
+                localStorage.removeItem("redirectUrl");
+              } else {
+                router.push("/");
+              }
+              setLoading(true);
+            } else if (res.data.message === "password not Match")
+              toast.warning(res.data.message);
+          });
+      },
+    });
+
+  if (loading) {
+    return <div>Loading....</div>;
+  }
+
+  return (
+    <main className="login-main">
+      <div className="login-box">
+        <div className="video-container">
+          <video width="100%" autoPlay muted loop>
+            <source src="/assets/video/comingsoon.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+
+        <div className="login-form">
+          <form onSubmit={handleSubmit}>
+            <div className="login__input-wrapper">
+              <div className="login__input-item">
+                <div className="login__input">
+                  <input
+                    name="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    type="email"
+                    placeholder="Enter your email"
+                    id="email"
+                  />
+                  <span>
+                    <UserThree />
+                  </span>
+                </div>
+                {touched.email && <ErrorMsg error={errors.email} />}
+              </div>
+
+              <div className="login__input-item">
+                <div className="login__input-item-inner p-relative">
+                  <div className="login__input">
+                    <input
+                      name="password"
+                      value={values.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      type={showPass ? "text" : "password"}
+                      placeholder="Password"
+                      id="password"
+                    />
+                    <span>
+                      <Lock />
+                    </span>
+                  </div>
+                  <span
+                    className="login-input-eye"
+                    onClick={() => setShowPass(!showPass)}
+                  >
+                    {showPass ? <i className="fa-regular fa-eye"></i> : <EyeCut />}
+                  </span>
+                </div>
+                {touched.password && <ErrorMsg error={errors.password} />}
+              </div>
+            </div>
+
+            <div className="login__option mb-25 d-sm-flex justify-content-between">
+              <div className="login__remember">
+                <input
+                  type="checkbox"
+                  checked={isCheckbox}
+                  onChange={handleChackbox}
+                  id="tp-remember"
+                />
+                <label htmlFor="tp-remember">Remember me</label>
+              </div>
+              <div className="login__forgot">
+                <Link href="/forgot-password">forgot password?</Link>
+              </div>
+            </div>
+            <div className="login__btn text-center">
+              <button
+                disabled={!isCheckbox}
+                type="submit"
+                className={isCheckbox == false ? "disabled-btn" : "fill-btn"}
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default LoginForm;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import React, { useState } from "react";
 // import Link from "next/link";
 // import { useFormik } from "formik";
@@ -510,155 +682,3 @@
 // export default LoginForm;
 
 
-
-import React, { useState } from "react";
-import Link from "next/link";
-import { useFormik } from "formik";
-import UserThree from "@svg/user-3";
-import Lock from "@svg/lock";
-import EyeCut from "@svg/eye-cut";
-import axios from "axios";
-import { toast } from "react-toastify";
-import { useRouter } from "next/router";
-import useGlobalContext from "@hooks/use-context";
-import { login_schema } from "@utils/validation-schema";
-import ErrorMsg from "./error-msg";
-
-const LoginForm = () => {
-  const { loading, setLoading } = useGlobalContext();
-  const [showPass, setShowPass] = useState(false);
-  const [isCheckbox, setIsCheckbox] = useState(false);
-  const router = useRouter();
-
-  const handleChackbox = () => {
-    setIsCheckbox(!isCheckbox);
-  };
-
-  // use formik
-  const { handleChange, handleSubmit, handleBlur, errors, values, touched } =
-    useFormik({
-      initialValues: {
-        email: "",
-        password: "",
-      },
-      validationSchema: login_schema,
-      onSubmit: (values, { resetForm }) => {
-        resetForm();
-        const userInfo = {
-          email: values.email,
-          password: values.password,
-        };
-
-        axios
-          .post(`${process.env.BASE_URL}user/login`, userInfo)
-          .then((res) => {
-            setLoading(false);
-            if (res.data.data) {
-              toast.success(res.data.message);
-              localStorage.setItem("accessToken", res.data.data);
-              const redirectUrl = localStorage.getItem("redirectUrl");
-              if (redirectUrl) {
-                router.push(redirectUrl);
-                localStorage.removeItem("redirectUrl");
-              } else {
-                router.push("/");
-              }
-              setLoading(true);
-            } else if (res.data.message === "password not Match")
-              toast.warning(res.data.message);
-          });
-      },
-    });
-
-  if (loading) {
-    return <div>Loading....</div>;
-  }
-
-  return (
-    <main className="login-main">
-      <div className="login-box">
-        <div className="login-form">
-          <form onSubmit={handleSubmit}>
-            <div className="login__input-wrapper">
-              <div className="login__input-item">
-                <div className="login__input">
-                  <input
-                    name="email"
-                    value={values.email}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    type="email"
-                    placeholder="Enter your email"
-                    id="email"
-                  />
-                  <span>
-                    <UserThree />
-                  </span>
-                </div>
-                {touched.email && <ErrorMsg error={errors.email} />}
-              </div>
-
-              <div className="login__input-item">
-                <div className="login__input-item-inner p-relative">
-                  <div className="login__input">
-                    <input
-                      name="password"
-                      value={values.password}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      type={showPass ? "text" : "password"}
-                      placeholder="Password"
-                      id="password"
-                    />
-                    <span>
-                      <Lock />
-                    </span>
-                  </div>
-                  <span
-                    className="login-input-eye"
-                    onClick={() => setShowPass(!showPass)}
-                  >
-                    {showPass ? <i className="fa-regular fa-eye"></i> : <EyeCut />}
-                  </span>
-                </div>
-                {touched.password && <ErrorMsg error={errors.password} />}
-              </div>
-            </div>
-
-            <div className="login__option mb-25 d-sm-flex justify-content-between">
-              <div className="login__remember">
-                <input
-                  type="checkbox"
-                  checked={isCheckbox}
-                  onChange={handleChackbox}
-                  id="tp-remember"
-                />
-                <label htmlFor="tp-remember">Remember me</label>
-              </div>
-              <div className="login__forgot">
-                <Link href="/forgot-password">forgot password?</Link>
-              </div>
-            </div>
-            <div className="login__btn text-center">
-              <button
-                disabled={!isCheckbox}
-                type="submit"
-                className={isCheckbox == false ? "disabled-btn" : "fill-btn"}
-              >
-                Sign In
-              </button>
-            </div>
-          </form>
-        </div>
-        <div className="video-container">
-          <video width="100%" autoPlay muted loop>
-            <source src="/assets/video/comingsoon.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-      </div>
-    </main>
-  );
-};
-
-export default LoginForm;
